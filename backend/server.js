d1eb2fb4854cef9cc0a5c5f59107c2987b93ce9b
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-04-23 14:04:03 +0300

    add controller, middleware, routes, error & exception handling

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware')
+
+const app = express()
+
+// Get data from body
+app.unsubscribe(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/',(req,res)=>{
+  res.status(201).json({message: 'Welcome to the Support Desk API'})
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
