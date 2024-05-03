8d9830633f23b4b2200082376d4df3784fa781ae
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-05-03 18:01:03 +0300

    add user login and logout

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -20,8 +20,33 @@ const register = async (userData) => {
   }
 };
 
+// Logout user
+const logout = () => {
+  localStorage.removeItem('user')
+}
+
+// Login user
+const login = async (userData) =>{
+  try {
+    const response = await axios.post(API_URL + '/login', userData);
+    if (response.data) {
+      // Store user data in local storage
+      localStorage.setItem('user', JSON.stringify(response.data));
+      return response.data;
+    } else {
+      throw new Error('Empty response from server');
+    }
+  } catch (error) {
+    // Handle errors
+    console.error('Registration failed:', error);
+    throw error;
+  }
+};
+
 const authService = {
   register,
+  logout,
+  login,
 }
 
 export default authService
