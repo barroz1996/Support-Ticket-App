a84af7f10b5bc9e67ecdf92cba7820fa27cc7f05
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-05-04 16:35:03 +0300

    add functionality of ticket

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import ticketSerive from './ticketService'
+import ticketService from './ticketService'
+
 
 
 const initialState = {
@@ -17,7 +18,38 @@ export const ticketSlice = createSlice({
   reducers: {
     reset: (state) => initialState
   },
-  extraReducers: (builder)=> {},
+  extraReducers: (builder)=> {
+    builder
+    .addCase(createTicket.pending, (state) => {
+      state.isLoading = true
+    })
+    .addCase(createTicket.fulfilled, (state) => {
+      state.isLoading = false
+      state.isSuccess= true
+    })
+    .addCase(createTicket.rejected, (state, action) => {
+      state.isLoading = false
+      state.isError = true
+      state.message = action.payload
+    })
+  },
+})
+
+// Create new ticket
+export const createTicket = createAsyncThunk('tickets/create',
+async(ticketData,thunkAPI)=>{
+  try{
+    const token = thunkAPI.getState().auth.user.token
+    return await ticketService.createTicket(ticketData, token)
+  }catch(error){
+    const message= (error.response && 
+      error.response.data && 
+      error.response.data.message)|| 
+    error.message || 
+    error.toString()
+
+    return thunkAPI.rejectWithValue(message)
+  }
 })
 
 export const {reset} = ticketSlice.actions
