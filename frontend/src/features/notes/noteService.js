7e7c9380616b96c5b03a7c55d3ff2fc4da942301
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-05-07 20:22:26 +0300

    Submit a note

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,22 +1,42 @@
 import axios from 'axios'
 
-const API_URL =  '/api/tickets/'
+const API_URL = '/api/tickets/'
 
 // Get ticket notes
-const getNotes = async(ticketId, token) => {
-  console.log("amoss")
+const getNotes = async (ticketId, token) => {
   const config = {
     headers: {
-      Authorization : `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   }
 
-  const response = await axios.get(API_URL + ticketId +'/notes', config)
+  const response = await axios.get(API_URL + ticketId + '/notes', config)
+
+  return response.data
+}
+
+// Create ticket note
+const createNote = async (noteText, ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+
+  const response = await axios.post(
+    API_URL + ticketId + '/notes',
+    {
+      text: noteText,
+    },
+    config
+  )
+
   return response.data
 }
 
 const noteService = {
   getNotes,
+  createNote,
 }
 
 export default noteService
--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,54 +1,87 @@
-import { useEffect } from "react"
-import { toast } from "react-toastify"
-import { useSelector ,useDispatch } from "react-redux"
-import { getTicket , closeTicket} from "../features/tickets/ticketSlice"
-import { getNotes, reset as notesReset } from "../features/notes/noteSlice"
-import { useParams , useNavigate} from "react-router-dom"
-import BackButton from "../components/BackButton"
-import Spinner from "../components/Spinner"
-import NoteItem from "../components/NoteItem"
-import moment from 'moment';
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import Modal from 'react-modal'
+import { FaPlus } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { getTicket, closeTicket } from '../features/tickets/ticketSlice'
+import { getNotes, createNote } from '../features/notes/noteSlice'
+import { useParams, useNavigate } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import NoteItem from '../components/NoteItem'
+
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+  },
+}
+
+Modal.setAppElement('#root')
 
 function Ticket() {
-  const {ticket, isLoading , isSuccess, isError, message} = useSelector((state) => state.tickets)
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [noteText, setNoteText] = useState('')
+  const { ticket } = useSelector((state) => state.tickets)
 
-  const {notes, isLoading : notesIsLoading} = useSelector((state) => state.notes)
+  const { notes } = useSelector((state) => state.notes)
 
-  const params = useParams()
+  // NOTE: no need for two useParams
+  // const params = useParams()
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  const {ticketId} = useParams()
+  const { ticketId } = useParams()
 
-  useEffect(() =>{
-    if(isError){
-      toast.error(message)
-    }
+  useEffect(() => {
+    dispatch(getTicket(ticketId)).unwrap().catch(toast.error)
+    dispatch(getNotes(ticketId)).unwrap().catch(toast.error)
+  }, [ticketId, dispatch])
 
-    dispatch(getTicket(ticketId))
-    dispatch(getNotes(ticketId))
-    // eslint-disable-next-line
-  },[isError, message, ticketId])
-
-  //Close ticket
+  // Close ticket
   const onTicketClose = () => {
+    // NOTE: we can unwrap our AsyncThunkACtion here so no need for isError and
+    // isSuccess state
     dispatch(closeTicket(ticketId))
-    toast.success('Ticket Closed')
-    navigate('/tickets')
+      .unwrap()
+      .then(() => {
+        toast.success('Ticket Closed')
+        navigate('/tickets')
+      })
+      .catch(toast.error)
   }
 
-  if(isLoading || notesIsLoading){
+  // Create note submit
+  const onNoteSubmit = (e) => {
+    // NOTE: we can unwrap our AsyncThunkACtion here so no need for isError and
+    // isSuccess state
+    e.preventDefault()
+    dispatch(createNote({ noteText, ticketId }))
+      .unwrap()
+      .then(() => {
+        setNoteText('')
+        closeModal()
+      })
+      .catch(toast.error)
+  }
+
+  // Open/close modal
+  const openModal = () => setModalIsOpen(true)
+  const closeModal = () => setModalIsOpen(false)
+
+  if (!ticket) {
     return <Spinner />
   }
 
-  if(isError){
-    return <h3>Something Went Worng</h3>
-  }
-  const formattedDate = moment(ticket.createAt).format('YYYY-MM-DD HH:mm:ss');
-
   return (
-    <div className="ticket-page">
-      <header className="ticket-header">
-        <BackButton url='/tickets' />
+    <div className='ticket-page'>
+      <header className='ticket-header'>
+        <BackButton />
         <h2>
           Ticket ID: {ticket._id}
           <span className={`status status-${ticket.status}`}>
@@ -56,21 +89,62 @@ function Ticket() {
           </span>
         </h2>
         <h3>
-          Date Submitted: {moment(ticket.createAt).format('YYYY-MM-DD HH:mm:ss')}
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}
         </h3>
         <h3>Product: {ticket.product}</h3>
         <hr />
-        <div className="ticket-desc">
+        <div className='ticket-desc'>
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
         <h2>Notes</h2>
       </header>
-      {notes.map((note)=> (
-        <NoteItem key={note._id} note = {note} />
-      ))}
+
       {ticket.status !== 'closed' && (
-        <button onClick={onTicketClose} className="btn btn-block btn-danger">Close Ticket</button>
+        <button onClick={openModal} className='btn'>
+          <FaPlus /> Add Note
+        </button>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel='Add Note'
+      >
+        <h2>Add Note</h2>
+        <button className='btn-close' onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={onNoteSubmit}>
+          <div className='form-group'>
+            <textarea
+              name='noteText'
+              id='noteText'
+              className='form-control'
+              placeholder='Note text'
+              value={noteText}
+              onChange={(e) => setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className='form-group'>
+            <button className='btn' type='submit'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {notes ? (
+        notes.map((note) => <NoteItem key={note._id} note={note} />)
+      ) : (
+        <Spinner />
+      )}
+
+      {ticket.status !== 'closed' && (
+        <button onClick={onTicketClose} className='btn btn-block btn-danger'>
+          Close Ticket
+        </button>
       )}
     </div>
   )
